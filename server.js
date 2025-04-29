29d46831e642257b793851b86e98921069668c11
seifeltohamy <st@keheilan>
2025-04-29 13:25:53 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
+const cors = require('cors'); 
 const fs = require('fs');
-const path = require('path');
-
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 
 const booksFile = path.join(__dirname, 'books.json');
