626706f9c75e89a11b60e01660fee29c9d3cba83
seifeltohamy <st@keheilan>
2025-04-29 13:33:09 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors'); 
 const fs = require('fs');
+const path = require('path'); 
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
